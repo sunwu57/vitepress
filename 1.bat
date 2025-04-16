98e5182d8666748b4cb191cf8612f78a9e8226bd
sunwu57 <1210396142@qq.com>
2025-04-16 11:03:39 +0800

    20250326-2

--- 1.bat
+++ 1.bat
@@ -1,30 +1,8 @@
-@echo off
-setlocal enabledelayedexpansion
-
-:: 设置控制台为 UTF-8 编码（可选）
-chcp 65001 >nul
-
-:: 获取当前日期（YYYYMMDD）
-for /f "tokens=2 delims==." %%i in ('"wmic os get localdatetime /value"') do set datetime=%%i
-set ymd=!datetime:~0,4!!datetime:~4,2!!datetime:~6,2!
-
-:: 获取备注编号
-set note=%1
-if "%note%"=="" (
-    set note=1
-)
-
-:: 拼接 commit 消息
-set message=%ymd%-%note%
-
-:: Git 操作
 pnpm run elog:sync-local
 git init
 git add .
-git commit -m "%message%"
+git commit -m "20250326-2"
 git remote set-url origin git@github.com:sunwu57/vitepress.git
 git branch -M main
 git config --global http.sslVerify "false"
 git push -u origin main --force
-:: 输出结果
-echo Commit 完成：%message% >> log.txt
